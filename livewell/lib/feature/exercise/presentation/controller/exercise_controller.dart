c657b5747c06e6505958df39e18d9987f5503a02
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2022-12-12 19:33:04 +0700

    testing data

--- livewell/lib/feature/exercise/presentation/controller/exercise_controller.dart
+++ livewell/lib/feature/exercise/presentation/controller/exercise_controller.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:health/health.dart';
+import 'package:livewell/core/local_storage/shared_pref.dart';
 import 'package:livewell/core/log.dart';
 import 'package:livewell/feature/exercise/domain/usecase/get_exercise_list.dart';
+import 'package:livewell/feature/exercise/domain/usecase/post_exercise_data.dart';
 import 'package:livewell/widgets/switcher/slide_switcher.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ExerciseController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -12,6 +19,7 @@ class ExerciseController extends GetxController
   ValueNotifier<int> sliderValueNotifier = ValueNotifier(0);
   Rx<num> steps = 0.obs;
   Rx<num> burntCalories = 0.0.obs;
+  TextEditingController dataController = TextEditingController();
 
   @override
   void onInit() {
@@ -22,6 +30,7 @@ class ExerciseController extends GetxController
     tabController.addListener(() {
       changeTab(ExerciseTab.values[tabController.index]);
     });
+    requestHealthAccess();
   }
 
   void changeTab(ExerciseTab tab) {
@@ -52,6 +61,71 @@ class ExerciseController extends GetxController
           0, (previousValue, element) => previousValue + (element.value ?? 0));
     });
     Log.colorGreen("total calories ${burntCalories.value}");
+  }
+
+  HealthFactory healthFactory = HealthFactory();
+
+  var types = [
+    HealthDataType.STEPS,
+    HealthDataType.ACTIVE_ENERGY_BURNED,
+    HealthDataType.SLEEP_ASLEEP,
+    HealthDataType.SLEEP_AWAKE,
+    HealthDataType.SLEEP_IN_BED,
+  ];
+
+  var permissions = [
+    HealthDataAccess.READ,
+    HealthDataAccess.READ,
+    HealthDataAccess.READ,
+    HealthDataAccess.READ,
+    HealthDataAccess.READ,
+  ];
+
+  void requestHealthAccess() async {
+    if (Platform.isAndroid) {
+      final permissionStatus = await Permission.activityRecognition.request();
+      if (permissionStatus.isGranted) {
+        fetchHealthDataFromTypes();
+        Log.colorGreen("Permission granted");
+      } else {
+        Log.error("Permission denied");
+      }
+    } else {
+      var isAllowed = await healthFactory.requestAuthorization(types,
+          permissions: permissions);
+      if (isAllowed) {
+        fetchHealthDataFromTypes();
+      }
+    }
+  }
+
+  void fetchHealthDataFromTypes() async {
+    var currentDate = DateTime(DateTime.now().year, DateTime.now().month,
+        DateTime.now().day, 0, 0, 0, 0, 0);
+    var dateTill = DateTime(DateTime.now().year, DateTime.now().month,
+        DateTime.now().day, 23, 59, 59, 0, 0);
+    List<HealthDataPoint> healthData = await healthFactory
+        .getHealthDataFromTypes(currentDate, dateTill, types);
+    Log.info(jsonEncode(healthData));
+    inspect(healthData);
+    PostExerciseData postExerciseData = PostExerciseData.instance();
+    var lastSyncHealth = await SharedPref.getLastHealthSyncDate();
+    // if user ever synced data
+    if (lastSyncHealth != null && healthData.isNotEmpty) {
+      healthData.sort((a, b) => a.dateFrom.compareTo(b.dateFrom));
+      var lastSyncDate = DateTime.parse(lastSyncHealth);
+      if (lastSyncDate.isBefore(healthData.last.dateTo)) {
+        var filteredHealth = healthData
+            .where((element) => element.dateTo.isAfter(lastSyncDate))
+            .toList();
+        dataController.text = jsonEncode(healthData);
+      } else {
+        Log.info("no new data");
+      }
+      // if user never synced data
+    } else if (healthData.isNotEmpty) {
+      dataController.text = jsonEncode(healthData);
+    }
   }
 }
 
