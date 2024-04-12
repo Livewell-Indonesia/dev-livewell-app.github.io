8ad0485054ad1f8fb86db5c2effc52c48f323679
Andi Asrafil <andiasrafil78@gmail.com>
2024-04-12 22:32:12 +0700

    remove unused files and dependencies

--- livewell/lib/core/service/service.dart
+++
@@ -1,6 +0,0 @@
-export 'common/common.dart';
-export 'internal_response.dart';
-export 'internal_service_option.dart';
-export 'internal_services.dart';
-export 'internal_services_exec.dart';
-export 'service_result.dart';
